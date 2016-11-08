e55f5ddc2e041dd797a10db3d96bdbe4371a4299
Cosmo Qiu <Cosmo Qiu>
2016-11-07 22:18:51 -0800

    initial commit for Section 6

---
+++ src/components/posts_index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
+
+class PostsIndex extends Component {
+
+    componentWillMount() {
+        console.log('this would be a good time to call an action creator to fetch posts.');
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        return this.props.posts.map((post) => {
+            return(
+                <li className="list-group-item" key={post.id}>
+                    <span className="pull-xs-right">{post.categories}</span>
+                    <strong>{post.title}</strong>
+                </li>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                        Add a new post
+                    </Link>
+                </div>
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
+            </div>
+        );
+    }
+}
+
+function mapStatetoProps(state) {
+    return { posts: state.posts.all }
+}
+
+export default connect(mapStatetoProps, { fetchPosts })(PostsIndex);
